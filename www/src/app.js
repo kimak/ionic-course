81ff1e143cc2c3c17013ce650a97f0a908d4b8e2
Kimak <dumand@ekino.com>
2015-06-23 23:49:44 +0200

    feat(app): rename angular module name

--- www/src/app.js
+++ www/src/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic'])
+angular.module('places', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
