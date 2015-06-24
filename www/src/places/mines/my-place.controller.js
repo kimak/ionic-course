04d2e456736b44c73a27961ae7067c2875e6017b
Kimak <dumand@ekino.com>
2015-06-24 17:29:17 +0200

    feat(my-place): add list data to work with

---
+++ www/src/places/mines/my-place.controller.js
@@ -0,0 +1,96 @@
+"use strict";
+
+angular.module('places')
+	.controller('MyPlaceCtrl', function($rootScope,$scope,$state) {
+		
+		//properties
+		$scope.list = [
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			},
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			},
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			},
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			},
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			},
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			},
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			},
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			},
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			},
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			},
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			},
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			},
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			},
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			},
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			},
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			},
+			{
+				name:'geneva',
+				description:'Cool place to see with great robots inside',
+				image:'http://lorempicsum.com/futurama/100/100'
+			}
+		];
+
+
+	});
