00089daa8479080661615186e41d24de4c1aec44
Kimak <dumand@ekino.com>
2015-06-25 17:28:59 +0200

    feat(more): add modal and component sample

---
+++ www/src/nav/tabs/tabs.controller.js
@@ -0,0 +1,18 @@
+"use strict";
+
+angular.module('places')
+	.controller('TabsCtrl', function($scope,$ionicModal) {
+
+		$scope.openModal= function(){
+
+			$ionicModal.fromTemplateUrl('src/nav/modal/more-modal.html', {
+				scope: $scope,
+				animation: 'slide-in-up'
+			}).then(function(modal) {
+				$scope.modal = modal;
+				$scope.modal.show();
+			});
+
+		}
+
+	});
